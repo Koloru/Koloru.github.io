5d19faffea018d94a2e90437862cf99387b93872
Koloru <sos.llyr@gmail.com>
2023-02-05 22:51:07 +0800

    Initial

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import "./App.css";
+import Button from "./components/Button";
+
+// bg from home
+import bg from "./assets/home/bg.jpg";
+import bg2 from "./assets/home/bg2.jpg";
+import bg3 from "./assets/home/bg3.jpg";
+
+function App() {
+	const navLinks = [
+		{ text: "Github", link: "https://github.com/Koloru" },
+		{ text: "LinkedIn", link: "https://www.linkedin.com/in/infanteneil/" },
+		{ text: "Email", link: "mailto:Koloru@Koloru.dev" },
+	];
+
+	const pictureArray = [bg, bg2, bg3];
+	const randomIndex = Math.floor(Math.random() * pictureArray.length);
+
+	return (
+		<div className='h-screen w-screen flex flex-col gap-4 items-center justify-center font-raleway text-white'>
+			<div
+				className='absolute h-screen w-screen -z-50 bg-cover bg-no-repeat bg-center'
+				style={{ backgroundImage: `url(${pictureArray[randomIndex]})` }}
+			></div>
+			<div className='absolute h-screen w-screen -z-20 bg-[#9fd8cb] opacity-40'></div>
+			<h1 className='font-bold text-4xl lg:text-6xl xl:text-9xl drop-shadow-lg shadow-emerald-300'>
+				Come back later
+			</h1>
+			<p className=''>This site is currently under construction</p>
+			<p className='text-4xl tracking-widest opacity-50 font-bold absolute bottom-[100px]'>
+				KOLORU
+			</p>
+			<div className='flex justify-between gap-2 absolute bottom-5'>
+				{navLinks.map((link) => (
+					<Button text={link.text} link={link.link} key={link.text} />
+				))}
+			</div>
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/Button.tsx
@@ -0,0 +1,21 @@
+interface type {
+	link: string;
+	text: string;
+}
+const Button = ({ link, text }: type) => {
+	return (
+		<a
+			href={link}
+			className='relative inline-flex items-center justify-start px-6 py-2 overflow-hidden font-bold group'
+		>
+			<span className='w-32 h-32 rotate-45 translate-x-12 -translate-y-2 absolute left-5 top-0 bg-white opacity-[3%]' />
+			<span className='absolute top-0 -left-5 w-48 h-48 -mt-1 transition-all duration-500 ease-in-out rotate-45 -translate-x-56 -translate-y-24 bg-white opacity-100 group-hover:-translate-x-8' />
+			<span className='relative w-full text-left text-white transition-colors duration-200 ease-in-out group-hover:text-[#4f359b]'>
+				{text}
+			</span>
+			<span className='absolute inset-0 rounded-full' />
+		</a>
+	);
+};
+
+export default Button;
