4733d5be4ff79f0465fc83142c5992cedf392b91
Koloru <sos.llyr@gmail.com>
2023-02-06 00:37:51 +0800

    favicon

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import vitePluginFaviconsInject from "vite-plugin-favicons-inject";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), vitePluginFaviconsInject("./src/assets/home/favicon.ico")],
 	base: "https://github.com/koloru/koloru.github.io",
 });
