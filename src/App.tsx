51e7f51642163cffc07f9163b7614b388f49c36c
Koloru <sos.llyr@gmail.com>
2023-02-14 20:57:44 +0800

    Start

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,14 @@
-import "./App.css";
-import Button from "./components/Button";
+import { Link, Route, Router, Switch } from "wouter";
+import cx from "classnames";
 
-// bg from home
-import bg from "./assets/home/bg.jpg";
-import bg2 from "./assets/home/bg2.jpg";
-import bg3 from "./assets/home/bg3.jpg";
+import "./App.css";
+
+import Construction from "./pages/Construction";
 
 function App() {
-	const navLinks = [
-		{ text: "Github", link: "https://github.com/Koloru" },
-		{ text: "LinkedIn", link: "https://www.linkedin.com/in/infanteneil/" },
-		{ text: "Email", link: "mailto:Koloru@Koloru.dev" },
-	];
-
-	const pictureArray = [bg, bg2, bg3];
-	const randomIndex = Math.floor(Math.random() * pictureArray.length);
-
+	const construction = true;
 	return (
-		<div className='h-screen w-screen flex flex-col gap-4 items-center justify-center font-raleway text-white'>
-			<div
-				className='absolute h-screen w-screen -z-50 bg-cover bg-no-repeat bg-center'
-				style={{ backgroundImage: `url(${pictureArray[randomIndex]})` }}
-			></div>
-			<div className='absolute h-screen w-screen -z-20 bg-[#9fd8cb] opacity-40'></div>
-			<h1 className='font-bold text-4xl lg:text-6xl xl:text-9xl drop-shadow-lg shadow-emerald-300'>
-				Come back later
-			</h1>
-			<p className=''>This site is currently under construction</p>
-			<p className='text-4xl tracking-widest opacity-50 font-bold absolute bottom-[100px]'>
-				KOLORU
-			</p>
-			<div className='flex justify-between gap-2 absolute bottom-5'>
-				{navLinks.map((link) => (
-					<Button text={link.text} link={link.link} key={link.text} />
-				))}
-			</div>
-		</div>
+		<div className={cx("h-screen", !construction && "bg-gray-800 p-4")}></div>
 	);
 }
 
