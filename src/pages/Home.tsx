d95738955850d506271d0587af9e3800d749228b
Neil <sos.llyr@gmail.com>
2023-03-20 06:03:23 +0000

    remove BR

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,11 +7,10 @@ const Home = () => {
 				Frontend Developer -{" "}
 				<span className='font-bold text-main'>Manila, Philippines</span>
 			</p>
-			<p className='text-gray2 font-light pl-1 w-2/4 text-left leading-loose'>
+			<p className='text-gray2 font-light pl-1 w-3/4 text-left leading-loose'>
 				I'm <span className='font-bold text-main mx-1'>WEB DEVELOPER</span> who
-				seeks novel solutions to novel problems. <br />
-				knowledge hungry and loves learning new things, I can help you build
-				your ideas to life.
+				seeks novel solutions to novel problems. Knowledge hungry and loves
+				learning new things, I can help you build your ideas to life.
 				<span className='block mt-6'>
 					I also like dogs and taking pictures of random places
 				</span>
