090df588aa1400b2ec908f2de84da0449cda302e
Koloru <sos.llyr@gmail.com>
2023-05-29 17:34:38 +0800

    fix navbar overflow

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import cx from "classnames";
 
 import "./App.css";
 
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,7 +4,7 @@ import NavSocials from "./NavSocials";
 
 const Navbar = () => {
   return (
-    <div className="my-auto relative flex flex-col items-center gap-12 text-main">
+    <div className="max-w-[25%] my-auto relative flex flex-col items-center gap-12 text-main">
       <div className="absolute right-0 top-[40%] w-[3px] h-40 bg-main rounded-full"></div>
 
       {/* Profile */}
@@ -17,20 +17,19 @@ const Navbar = () => {
           />
         </div>
         <h1 className="text-4xl tracking-widest font-bold uppercase">Koloru</h1>
-        <p className="font-light text-sm">
-          I'm jealous of the things people create
+        <p className="font-light text-sm max-w-[55%] max-h-48 text-center overflow-y-hidden">
+          Frontend Developer who likes to take random pictures
         </p>
         {/* Navlinks */}
         <div className="block bg-main w-[25px] h-[1px] mt-4"></div>
-        <ul className="flex flex-col gap-4 text-center w-3/5 mt-8 ">
+        <ul className="flex flex-col gap-4 text-center w-3/5 mt-4 ">
           <NavLink text="Home" url="/" />
+          <NavLink text="Stuff I made" url="projects" />
+          <NavLink text="Gallery" url="gallery" />
           <NavLink text="My Resume" url="https://resume.koloru.dev/" />
-          <NavLink text="Stuff I'm Learning" url="projects" />
-          <NavLink text="Random pictures I take" url="gallery" />
         </ul>
       </div>
-			<NavSocials />
-
+      <NavSocials />
     </div>
   );
 };
