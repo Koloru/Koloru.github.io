426dc377feb45f8144657f971d7740b41a2fd209
Koloru <sos.llyr@gmail.com>
2023-05-30 01:10:33 +0800

    add project card

--- src/components/Projects/ProjectCard.tsx
+++ src/components/Projects/ProjectCard.tsx
@@ -1,6 +1,36 @@
+import { AiFillGithub } from "react-icons/ai";
+
+interface props {
+  title: string;
+  link: string;
+  image: string;
+  github: string;
+}
+
 const ProjectCard = () => {
   return (
-    <div>ProjectCard</div>
-  )
-}
-export default ProjectCard
+    <a
+      href="https://i.imgur.com/Bl0jjby.jpeg"
+      className="relative text-white flex flex-col w-full max-w-[250px] h-[300px] max-h-[300px] p-4 rounded-md cursor-pointer hover:scale-125 duration-200 ease-in-out"
+    >
+      <div className="absolute top-0 bottom-0 left-0 right-0 bg-red-500 -z-50 rounded-md overflow-hidden brightness-50">
+        <img
+          src="https://i.imgur.com/Bl0jjby.jpeg"
+          className="h-full w-full block aspect-auto"
+        />
+      </div>
+      <div className="flex justify-between mt-auto">
+        <div className="text-lg">Idolmaster Api</div>
+        <div className="flex gap-2 items-center text-3xl">
+          <a
+            href="https://www.google.com/"
+            className="hover:text-blue-500 ease-in duration-200"
+          >
+            <AiFillGithub />
+          </a>
+        </div>
+      </div>
+    </a>
+  );
+};
+export default ProjectCard;
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -4,8 +4,8 @@ ProjectCard;
 const Projects = () => {
   return (
     <div className="flex flex-col gap-y-5 justify-center w-full h-full py-16 pl-24">
-      <h1 className="text-5xl">Stuff that I made</h1>
-      <div className="flex gap-5">
+      <h1 className="text-5xl mb-4">Stuff that I made</h1>
+      <div className="flex flex-wrap items-center gap-6 max-w-[80%]">
         <ProjectCard />
         <ProjectCard />
         <ProjectCard />
