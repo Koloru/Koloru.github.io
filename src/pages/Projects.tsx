68e35619960ffd4a7e86be5adcb03e446680b06d
Koloru <sos.llyr@gmail.com>
2023-05-31 03:29:20 +0800

    add gap to cards in project page

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -6,7 +6,7 @@ const Projects = () => {
   return (
     <div className="flex flex-col justify-center w-full h-full py-16 pl-24 gap-y-5">
       <h1 className="mb-4 text-5xl">Stuff that I made</h1>
-      <div className="flex flex-wrap items-center gap-2 max-w-[80%] ">
+      <div className="flex flex-wrap items-center gap-4 max-w-[80%] ">
         {projects.map((project) => (
           <ProjectCard key={project.id} {...project} />
         ))}
